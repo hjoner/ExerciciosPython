174fb00536e49dc8f0dfaaf3b630d2acadd64198
hjoner <hjoner@gmail.com>
2018-11-26 12:07:40 -0200

    Novo commit!

--- exercicio57.py
+++ exercicio57.py
@@ -9,22 +9,30 @@ Data:26/11/2018
 
 n = 0
 
-x = 0
+inicio = 0
+
+fim = 0
+
 
 #Entrada de dados
 
-x = 1
+inicio = int(input("Informe o número onde a tabuada deve iniciar: "))
+
+fim = int(input("Informe onde a tabuada deve finalizar: "))
 
 n = int(input("Informe o número que você deseja obter a tabuada: "))
 
+x = inicio
 
 #Processamento de dados
 
 
 
-while x <= 10:
+while x <= fim and x >= inicio:
  
     print("%d X %d = %d" % (n, x, n * x))
     x = x + 1
+
+
 #Saída de dados
 
