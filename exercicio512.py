15559c3ae330c6e4b271039186696a731dd9e6e4
hjoner <hjoner@gmail.com>
2018-11-27 23:13:58 -0200

    novo commit!

--- exercicio512.py
+++ exercicio512.py
@@ -1,3 +1,4 @@
+
 """
 Descrição: Este programa calcula o  rendimento de uma aplicação, com depósitos mensais.
 Autor:Henrique Joner
@@ -17,6 +18,10 @@ n = 0
 
 juros = 0
 
+saldo = 0
+
+rendimento = 0
+
 #Entrada de dados
 
 
@@ -26,17 +31,22 @@ mensal = float(input("Informe o valor dos depósitos mensais: "))
 
 taxa = float(input("Informe a taxa de juros mensal: "))
 
-n = 0
+n = 1
 
 juros = 0
 
+saldo = deposito
+
+rendimento = saldo - deposito
+
 #Processamento de dados
 
-while n < 24:
-    deposito = deposito + juros + mensal
+while n <= 24:
+    juros = (saldo * taxa/100)
+    saldo = saldo + juros + mensal
     n = n + 1
-    juros =  (deposito * taxa/100)
-    print("O seu saldo no mês %d é de R$%5.2f e o seu rendimento total foi de R$ %5.2f" % (n, deposito, juros))
+    rendimento = saldo - deposito
+    print("O seu saldo no mês %d é de R$%5.2f e o seu rendimento total foi de R$ %5.2f" % (n, saldo, rendimento))
 
 
 #Saída de dados
--- exercicio513.py
+++ exercicio513.py
@@ -7,7 +7,7 @@ Data:26/11/2018
 
 #Inicialização de variáveis
 
-mensal = 0
+parcela = 0
 
 divida = 0
 
@@ -17,27 +17,41 @@ n = 0
 
 juros = 0
 
+saldo = 0
+
+amortizacao = 0
+
+jurostotal = 0
+
 #Entrada de dados
 
 
 divida = float(input("Qual o valor da dívida? "))
 
-mensal = float(input("Informe o valor da parcela a pagar todos os meses: "))
+parcela = float(input("Informe o valor da parcela a pagar todos os meses: "))
 
 taxa = float(input("Informe a taxa de juros mensal: "))
 
-n = 0
+n = 1
 
 juros = 0
 
+saldo = divida
+
+amortizacao = 0
+
+jurostotal = 0
+
 #Processamento de dados
 
-while n < 24:
-    deposito = deposito + juros + mensal
+while n <= (divida + saldo)/parcela :
+    juros = (saldo * taxa/100)
+    jurostotal = jurostotal + juros
+    saldo = saldo + juros - parcela
     n = n + 1
-    juros =  (deposito * taxa/100)
-    print("O seu saldo no mês %d é de R$%5.2f e o seu rendimento total foi de R$ %5.2f" % (n, deposito, juros))
-
+    amortizacao = divida - (saldo - jurostotal)
+    print("Após o pagamento da parcela relativa ao %dº mês você já pagou R$ %5.2f referente à amortização, o saldo a ser quitado é de R$  %5.2f!" % (n, amortizacao, saldo))
 
 #Saída de dados
+print("Você levará %d meses para quitar sua dívida e o total de juros que você pagará é de R$ %5.2f" % (n, jurostotal))
 
